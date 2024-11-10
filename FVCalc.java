8a0501e3dab500935442e84095e6d904f9eee224
roniklinger <roni.klinger@post.runi.ac.il>
2024-11-10 10:40:03 +0200

    check 2

--- FVCalc.java
+++ FVCalc.java
@@ -2,12 +2,12 @@
 
 
 public class FVCalc {
-	public static void main(String[] args){
+	public static void main(String[] args)
+	{
 		int currentValue = Integer.parseInt(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
-
-		System.out.println(currentValue * (Math.pow((1 + rate/100),n)));
-		
+		int futureValue = (int)(currentValue * (Math.pow((1 + rate/100),n)));
+		System.out.println("After " + n + " years, " + currentValue + "$ saved at " + rate + "% will yield " + futureValue + "$");
 	}
 }
